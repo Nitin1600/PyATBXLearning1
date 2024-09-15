458862ae647a39da12159e5597acd046da97be14
Nitin1600 <nitgpatil1@gmail.com>
2024-09-15 21:36:08 +0530

    All the files of August_2024

---
+++ src/August/ex_08082024/Lab002M.py
@@ -0,0 +1,14 @@
+# age = 25
+# print(type(age))
+
+# age = "twenty-five"
+# print(type(age))
+
+#print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+
+# with open("output.txt", "w") as file:
+#     print("Hello", "World", sep="-", end="!", file=file, flush=True)
+
+# print(50, 1000, 3.142, "Hello World")
+print(99)
+print("Hello World")
---
+++ src/August/ex_10082024/Lab006.py
@@ -0,0 +1,5 @@
+#Program to find the Max number between two integers.
+#34, 100 -> 100
+
+print(max(34,100,101))
+print(max(10, 23, 45, -1, -2, 200, 1, 87, 34))
